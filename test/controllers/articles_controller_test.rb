aec22d5155d4f38fd6c022e4bbba65ae6ce0ecf5
nikhilbhatt <nikhilbhatt931@gmail.com>
2024-01-06 17:57:51 +0530

    Create github workflows

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,38 +1,57 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get articles_index_url
+  test 'should get index' do
+    get articles_url
+
+    assert_equal 'index', @controller.action_name
+    assert_match 'Articles', @response.body
+  end
+
+  test 'should show article' do
+    article = articles(:one)
+    get article_url(article)
     assert_response :success
   end
 
-  test "should get show" do
-    get articles_show_url
+  test 'should get new' do
+    get new_article_url
     assert_response :success
   end
 
-  test "should get new" do
-    get articles_new_url
+  test 'should create a article' do
+    assert_difference('Article.count', 1) do
+      post articles_path, params: { article: { title: 'New article', body: 'New article Body' } }
+    end
+
+    assert_response :redirect
+  end
+
+  test 'should get edit' do
+    article = articles(:one)
+
+    get edit_article_url(article)
+
     assert_response :success
   end
 
-  test "should get create" do
-    get articles_create_url
-    assert_response :success
+  test 'should update article' do
+    article = articles(:one)
+
+    patch article_url(article), params: { article: { title: 'updated' } }
+
+    assert_redirected_to article_path(article)
+
+    article.reload
+    assert_equal 'updated', article.title
   end
 
-  test "should get edit" do
-    get articles_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get articles_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get articles_destroy_url
-    assert_response :success
+  test 'should destroy article' do
+    article = articles(:two)
+    assert_difference('Article.count', -1) do
+      delete article_url(article)
+    end
   end
 end
