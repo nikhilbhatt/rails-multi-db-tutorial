6c9295cfc2a462c04ce02cf18f8881def790e4f2
nikhilbhatt <nikhilbhatt931@gmail.com>
2024-05-05 23:17:49 +0530

    Lint fixes

--- app/middleware/sidekiq_adapter.rb
+++ app/middleware/sidekiq_adapter.rb
@@ -1,20 +1,17 @@
 # frozen_string_literal: true
+class SidekiqAdapter
+  include Sidekiq::ServerMiddleware
 
-module Middleware
-  class SidekiqAdapter
-    include Sidekiq::ServerMiddleware
-
-    def call(job_instance, job_payload, queue)
-      shard = current_shard(job_payload)
-      ApplicationRecord.connected_to(shard: shard, role: :writing) do
-        yield
-      end
-    rescue StandardError => e
-      p "Error occured #{e}"
+  def call(job_instance, job_payload, queue)
+    shard = current_shard(job_payload)
+    ApplicationRecord.connected_to(shard: shard, role: :writing) do
+      yield
     end
+  rescue StandardError => e
+    p "Error occured #{e}"
+  end
 
-    def current_shard(job_payload)
-      job_payload.try(:[], 'cattr').try(:[], 'tenant')&.to_sym
-    end
+  def current_shard(job_payload)
+    job_payload.try(:[], 'cattr').try(:[], 'tenant')&.to_sym
   end
 end
